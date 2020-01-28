3b7d398009eced102800699f9b1f17f865f7d38b
nemoto <x0703x@gmail.com>
2020-01-28 16:49:19 +0900

    Create UI MenuWIndow

---
+++ Assets/Script/Field/UI/WindowSystem.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindowSystem : MonoBehaviour
+{
+    public GameObject MenuWindow;
+    public bool canOpen = true;
+    public static WindowSystem instance;
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!canOpen) return;
+
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            if (MenuWindow.activeSelf) MenuWindow.SetActive(false);
+            else MenuWindow.SetActive(true);
+        }
+    }
+}
