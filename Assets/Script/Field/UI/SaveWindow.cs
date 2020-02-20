cdd57e012ba46f11510429a52096352619f39ed2
nemoto <x0703x@gmail.com>
2020-02-20 15:49:21 +0900

    new script SaveWindow.cs,MenuWindow add SaveWindow

---
+++ Assets/Script/Field/UI/SaveWindow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveWindow : BaseWindow
+{
+    public override void Open()
+    {
+        base.Open();
+        MenuWindow.instance.currentWindow = this;
+    }
+
+    public override void Close()
+    {
+        base.Close();
+    }
+
+    public override void Cancel()
+    {
+        MenuWindow.instance.currentWindow = MenuWindow.instance;
+        Close();
+    }
+}
