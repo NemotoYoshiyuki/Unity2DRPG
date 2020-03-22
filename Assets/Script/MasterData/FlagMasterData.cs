7d8e1dba3791caa2739918b4cd64f275d4ed064d
nemoto <x0703x@gmail.com>
2020-03-22 22:54:56 +0900

    scriptableobject で MasterData　を定義

---
+++ Assets/Script/MasterData/FlagMasterData.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu()]
+public class FlagMasterData : ScriptableObject
+{
+    public List<Flag> flags = new List<Flag>();
+}
---
+++ Assets/Script/MasterData/ItemMasterData.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu()]
+public class ItemMasterData : ScriptableObject
+{
+    public List<ItemData> items = new List<ItemData>();
+}
