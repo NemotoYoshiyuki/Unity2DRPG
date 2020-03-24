be6036b4fd6ad1a927f2a5c4597be6eb74e9e229
nemoto <x0703x@gmail.com>
2020-03-24 16:27:22 +0900

    MessageSystem.cs Create Singleton

--- Assets/Script/Battle/UI/BattleMessage.cs
+++ Assets/Script/Battle/UI/BattleMessage.cs
@@ -16,5 +16,4 @@ public class BattleMessage : MonoBehaviour
     {
         return instance.messageWindow;
     }
-
 }
--- Assets/Script/MessageSystem.cs
+++ Assets/Script/MessageSystem.cs
@@ -7,19 +7,46 @@ public class MessageSystem : MonoBehaviour
     public MessageWindow messageWindow;
     public ChoiceWindow choiceWindow;
     public static MessageSystem instance;
+    public static MessageSystem Instance
+    {
+        get
+        {
+            if (instance != null)
+                return instance;
+
+            instance = FindObjectOfType<MessageSystem>();
+
+            if (instance != null)
+                return instance;
+
+            Create();
+
+            return instance;
+        }
+    }
 
     private void Awake()
     {
-        instance = this;
+        if (Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+    }
+
+    protected static void Create()
+    {
+        MessageSystem controllerPrefab = Resources.Load<MessageSystem>("MessageSystem");
+        instance = Instantiate(controllerPrefab);
     }
 
     public static MessageWindow GetWindow()
     {
-        return instance.messageWindow;
+        return Instance.messageWindow;
     }
 
     public static ChoiceWindow GetChoise()
     {
-        return instance.choiceWindow;
+        return Instance.choiceWindow;
     }
 }
