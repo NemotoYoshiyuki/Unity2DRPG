60a4960b07bb1093dada50e4fb4a95dfe27cd449
nemoto <x0703x@gmail.com>
2020-04-14 22:43:19 +0900

    リファクタリング

--- Assets/Script/Field/NPC/Conversation.cs
+++ Assets/Script/Field/NPC/Conversation.cs
@@ -2,7 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Conversation : MonoBehaviour
+public class Conversation : Interactable
 {
     public List<string> conversation;
+
+    public override void OnInteractable()
+    {
+        StartCoroutine(Talk());
+    }
+
+    public IEnumerator Talk()
+    {
+        MessageWindow messageWindow = MessageSystem.GetWindow();
+
+        PlayerInteract.InteractableStart();
+
+        //会話文章の表示
+        //display talk
+        List<string> content = conversation;
+        foreach (var item in content)
+        {
+            yield return StartCoroutine(messageWindow.ShowClick(item));
+        }
+        messageWindow.Close();
+
+        PlayerInteract.InteractableEnd();
+    }
 }
--- Assets/Script/Field/NPC/NPC.cs
+++ Assets/Script/Field/NPC/NPC.cs
@@ -2,36 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NPC : Interactable
+public class NPC : MonoBehaviour
 {
 
     // Start is called before the first frame update
     void Start()
     {
         gameObject.layer = LayerMask.NameToLayer("NPC");
-    }
-
-    public override void OnInteractable()
-    {
-        StartCoroutine(Talk());
-    }
-
-    public IEnumerator Talk()
-    {
-        MessageWindow messageWindow = MessageSystem.GetWindow();
-        Conversation conversation = GetComponent<Conversation>();
-
-        PlayerInteract.InteractableStart();
-
-        //会話文章の表示
-        //display talk
-        List<string> content = conversation.conversation;
-        foreach (var item in content)
-        {
-            yield return StartCoroutine(messageWindow.ShowClick(item));
-        }
-        messageWindow.Close();
-
-        PlayerInteract.InteractableEnd();
     }
 }
