cfc65e1b9af836db8340dac4257849dd8f51ce4d
nemoto <x0703x@gmail.com>
2020-06-20 23:58:36 +0900

    追加

---
+++ Assets/TEst/バフ・デバフ/AddBuffEffect.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddBuffEffect : CommandEffect
+{
+    public StatusType statusType;
+    public int count;
+    public int value;
+
+    private EffectInfo effectInfo;
+
+    public override void SetUp(EffectInfo effectInfo)
+    {
+        this.effectInfo = effectInfo;
+        effectInfo.characteristic = EffectInfo.Characteristic.特殊;
+        effectInfo.effectType = EffectInfo.EffectType.補助;
+    }
+
+    public override void Use(BattleCharacter owner, BattleCharacter target)
+    {
+
+        BattleDirectorController.Instance.AddBuff(target, new Buff(statusType, count, value));
+
+        if (value >= 0)
+        {
+            BattleDirectorController.Instance.Message($"{target.CharacterName}の{statusType.ToString()}が　あがった");
+        }
+        else
+        {
+            BattleDirectorController.Instance.Message($"{target.CharacterName}の{statusType.ToString()}が　さがった");
+        }
+    }
+}
