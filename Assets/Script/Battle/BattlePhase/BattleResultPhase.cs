ebcc8108c22c8f2a6a4295312c2776f9e5bd571c
nemoto <x0703x@gmail.com>
2020-07-09 22:03:41 +0900

    レベルアップ処理実装

--- Assets/Script/Battle/BattlePhase/BattleResultPhase.cs
+++ Assets/Script/Battle/BattlePhase/BattleResultPhase.cs
@@ -13,35 +13,93 @@ public class BattleResultPhase : MonoBehaviour
         {
             alivePlayer.status.exp = dropExp;
             string ms = alivePlayer.CharacterName + "は" + dropExp + "けいけんちをかくとく";
-            //経験値加算
-            Party.Find(alivePlayer.playerData.CharacterID).exp += dropExp;
-            //レベルがあがったら体力全回復
             yield return StartCoroutine(BattleMessage.GetWindow().ShowClick(ms));
+
+            //経験値加算
+            //Party.Find(alivePlayer.playerData.CharacterID).exp += dropExp;
+            //テスト
+            int lv = Party.Find(alivePlayer.playerData.CharacterID).lv;
+            yield return Hoge(Party.Find(alivePlayer.playerData.CharacterID), dropExp);
+            if (lv != Party.Find(alivePlayer.playerData.CharacterID).lv)
+            {
+                //レベルがあがったら体力全回復
+                CharacterData data = Party.Find(alivePlayer.playerData.CharacterID);
+                alivePlayer.status.hp = data.status.maxHp;
+                alivePlayer.status.mp = data.status.maxMp;
+            }
         }
 
-
-
-        //2020/06/13　この下の処理が正しく行われず再度バトルに突入したときステータスが引き継がれない
-        Debug.Log("ばぐばぐ");
-
-        //レベルアップ処理
-        //基礎ステータスに反映
         //基礎ステータスにバトルで損傷したHPとMPを反映する
         foreach (var item in BattleController.instance.playerCharacters)
         {
-            CharacterData characterData =Party.Find(item.playerData.CharacterID);
-            Debug.Log(characterData.status.hp);
+            CharacterData characterData = Party.Find(item.playerData.CharacterID);
             characterData.status.hp = item.battleStaus.Status.hp;
-            //characterData.status.hp = 777;
-            Debug.Log(item.battleStaus.Status.hp);
             characterData.status.mp = item.battleStaus.Status.mp;
 
             //死亡したキャラはHP１で復活
-            if (item.battleStaus.Status.hp < 0)
+            if (characterData.status.hp < 0)
             {
                 characterData.status.hp = 1;
             }
         }
+        yield break;
+    }
+
+    public IEnumerator Hoge(CharacterData characterData, int exp)
+    {
+        characterData.exp += exp;
+        while (IsLevelUp(characterData))
+        {
+            yield return LevelUp(characterData);
+        }
+        yield break;
+    }
+
+    private bool IsLevelUp(CharacterData characterData)
+    {
+        int level = characterData.lv;
+        int exp = characterData.exp;
+        List<int> experienceTable = ExperienceTable.Get();
+        int nextLevel = experienceTable[level + 1];
+        if (exp > nextLevel) return true;
+        return false;
+    }
+
+    public IEnumerator LevelUp(CharacterData characterData)
+    {
+        List<string> ms = new List<string>();
+
+        characterData.lv++;
+        ms.Add(characterData.GetName() + "の レベルがあがった\nレベルが" + characterData.lv + "になった");
+
+        string m = "{0}が{1}あがった";
+        Status growth = characterData.playerData.GrowthRate;
+        if (growth.maxHp != 0)
+        {
+            characterData.status.maxHp += growth.maxHp;
+            ms.Add(string.Format(m, "体力", growth.maxHp.ToString()));
+        }
+        if (growth.maxMp != 0)
+        {
+            characterData.status.maxMp += growth.maxMp;
+            ms.Add(string.Format(m, "MP", growth.maxMp.ToString()));
+        }
+        if (growth.attack != 0)
+        {
+            characterData.status.attack += growth.attack;
+            ms.Add(string.Format(m, "攻撃", growth.attack.ToString()));
+        }
+        if (growth.deffence != 0)
+        {
+            characterData.status.deffence += growth.deffence;
+            ms.Add(string.Format(m, "防御", growth.deffence.ToString()));
+        }
+        if (growth.speed != 0)
+        {
+            characterData.status.speed += growth.speed;
+            ms.Add(string.Format(m, "速さ", growth.speed.ToString()));
+        }
+        yield return BattleMessage.GetWindow().ShowClick(ms);
         yield break;
     }
 }
