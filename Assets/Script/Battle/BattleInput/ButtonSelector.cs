991ae612d2aea05e82a2acfe865ac34b98dea040
nemoto <x0703x@gmail.com>
2020-07-24 16:34:28 +0900

    バトルコマンドをキー入力に対応

---
+++ Assets/Script/Battle/BattleInput/ButtonSelector.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ButtonSelector : MonoBehaviour
+{
+    public Button button;
+    // Start is called before the first frame update
+    void OnEnable()
+    {
+        StartCoroutine(OnRendering());
+        IEnumerator OnRendering()
+        {
+            //Buttonのレンダリング完了するまで待つ必要がある
+            yield return new WaitForEndOfFrame();
+            //すでにセレクト状態のボタンはSelect()メソッドを使用しても何起きない
+            //そのため、一度ボタンのセレクトを解除する必要がある
+            EventSystem.current.SetSelectedGameObject(null);
+            button.Select();
+            yield return null;
+        }
+    }
+}
