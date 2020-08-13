107a3235d2ee443ae4ca1a486003c9478f6f98df
nemoto <x0703x@gmail.com>
2020-08-14 00:26:39 +0900

    エディター上でセーブデータを保存する形式を変更

--- Assets/Script/SaveData/JsonSerializer.cs
+++ Assets/Script/SaveData/JsonSerializer.cs
@@ -6,7 +6,12 @@ public static class JsonSerializer
     public static void Save(object obj, string fileName)
     {
         //各クラスをjsonにする
+#if UNITY_EDITOR
+        var json = UnityEditor.EditorJsonUtility.ToJson(obj, true);
+#else
         var json = JsonUtility.ToJson(obj, true);
+#endif
+
         var filePath = GetFilePath(fileName);
         File.WriteAllText(filePath, json);
 
@@ -23,18 +28,27 @@ public static class JsonSerializer
             return;
         }
         string jsonStr = File.ReadAllText(filePath);
+#if UNITY_EDITOR
+        UnityEditor.EditorJsonUtility.FromJsonOverwrite(jsonStr, obj);
+#else
         JsonUtility.FromJsonOverwrite(jsonStr, obj);
+#endif
     }
 
     private static string GetFilePath(string fileName)
     {
+#if UNITY_EDITOR
+        string directoryPath = Application.persistentDataPath + "/" + "SaveData_Editor";
+#else
         string directoryPath = Application.persistentDataPath + "/" + "SaveData";
+#endif
 
         //ディレクトリが無ければ作成します
         if (!Directory.Exists(directoryPath))
         {
             Directory.CreateDirectory(directoryPath);
         }
+
         string filePath = directoryPath + "/" + fileName;
         return filePath;
     }
