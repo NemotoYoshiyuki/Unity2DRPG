788b349a3d97d0d1cae8c4b9e5750e540dac3647
nemoto <x0703x@gmail.com>
2020-08-15 18:39:25 +0900

    FPS計測スクリプト追加

---
+++ Assets/FPSCounter.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class FPSCounter : MonoBehaviour
+{
+    public Text FpsText;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.frameCount % Application.targetFrameRate == 0)
+        {
+            FpsText.text = "FPS:" + (int)(1 / Time.deltaTime);
+        }
+    }
+}
